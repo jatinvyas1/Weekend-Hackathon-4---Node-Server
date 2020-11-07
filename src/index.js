8eb34db788e7ab764dfac6aaf2e5851d7acb9676
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-07 18:27:11 +0530

    c

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ const httpServer = http.createServer(handleServer).listen(8081);
 function handleServer(req, res) {
   var url = req.url;
   if (url === "/welcome") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
+    res.writeHead(200, { "Content-Type": "text/plain" });
     res.write("Welcome to Dominos!");
-    res.send();
+    res.end();
   } else if (url === "/contact") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.write({
       phone: "18602100000",
       email: "guestcaredominos@jublfood.com"
     });
-    res.send();
+    res.end();
   } else {
     res.statusCode = 404;
     res.end();
