23c811617af76b684d8634f8f6356a1b9ce9d3ab
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-07 17:52:45 +0530

    c

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ function handleServer(req, res) {
     res.end();
   } else if (url === "/contact") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.write("Welcome to Dominos's!");
+    res.write({
+      phone: "18602100000",
+      email: "guestcaredominos@jublfood.com"
+    });
     res.end();
   } else {
     res.statusCode = 404;
