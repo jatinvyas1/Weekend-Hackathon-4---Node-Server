61cd5d6bdaa40c87a6084a4b902f5bb29d80fdd5
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-07 17:52:23 +0530

    c

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  var url = req.url;
+  if (url === "/welcome") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Welcome to Dominos's!");
+    res.end();
+  } else if (url === "/contact") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write("Welcome to Dominos's!");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 }
 
 module.exports = httpServer;
